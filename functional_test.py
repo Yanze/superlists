53404887c4429445999b5d03e508b63b4c4885c0
Yanze LIU <yanzeliu.france@gmail.com>
2016-06-28 12:37:55 -0700

    functional test now checks we can input a to-do item

--- functional_test.py
+++ functional_test.py
@@ -21,6 +21,31 @@ class NewVisitorTest(unittest.TestCase):
 
         # notice that the page title and header mention 'to-do lists'
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.asserIn('To-Do', header_text)
+
+        # to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_tag_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # typing 'Buy peacock feathers' into a text box
+        inputbox.send_keys('Buy peacock feathers')
+        # when hitting the enter key, the page updates, and now the page lists has an to-do item in to-do list table
+        inputbox.send_keys(keys.ENTER)
+
+        # find_element_by， return one element, or rases an exception if it can't find it
+        # find_elements_by, return one list, can be an empty list
+        table = self.browser.find_element_by_tag_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        # be able to enter another to-do item
+
 
         # Stop the test run on the first error or failure.
         self.fail('Finish the test!')
