5e3e508226155f94cc3e5706bbd75eb69552cb47
Yanze LIU <yanzeliu.france@gmail.com>
2016-06-28 18:09:09 -0700

    fix all small bugs and upgrade firefox and selenium

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,6 @@
 # webdriver is a tool for automating web application testing and in particular to verify that they work as expected/
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -22,10 +23,10 @@ class NewVisitorTest(unittest.TestCase):
         # notice that the page title and header mention 'to-do lists'
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.asserIn('To-Do', header_text)
+        self.assertIn('To-Do', header_text)
 
         # to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_tag_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -34,14 +35,15 @@ class NewVisitorTest(unittest.TestCase):
         # typing 'Buy peacock feathers' into a text box
         inputbox.send_keys('Buy peacock feathers')
         # when hitting the enter key, the page updates, and now the page lists has an to-do item in to-do list table
-        inputbox.send_keys(keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
 
         # find_element_by， return one element, or rases an exception if it can't find it
         # find_elements_by, return one list, can be an empty list
-        table = self.browser.find_element_by_tag_id('id_list_table')
+        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # be able to enter another to-do item
